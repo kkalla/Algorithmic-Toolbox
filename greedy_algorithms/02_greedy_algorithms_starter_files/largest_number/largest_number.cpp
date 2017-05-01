c914a223d2c40ced28c0d5004c3706c1b07c1638
Kkalla <dkswns333@gmail.com>
2017-05-02 01:43:21 +0900

    Complete prob#6
    -Update fuctions in starter file of prob#6 (largest_number)
    -Need some stress tests

--- greedy_algorithms/02_greedy_algorithms_starter_files/largest_number/largest_number.cpp
+++ greedy_algorithms/02_greedy_algorithms_starter_files/largest_number/largest_number.cpp
@@ -7,11 +7,39 @@
 using std::vector;
 using std::string;
 
+bool IsGreaterOrEqual(std::string digit,std::string maxdigit){
+	for(size_t i =0;i<std::min(digit.size(),maxdigit.size());i++){
+		if(digit.substr(i,1).compare(maxdigit.substr(i,1))>0)
+			return true;
+	}
+	if(digit.size()<maxdigit.size())
+		return true;
+	else
+		return false;
+
+}
+
 string largest_number(vector<string> a) {
-  //write your code here
+  vector<string> b;
+  while(a.size()){
+	  std::string maxdigit=a[0];
+	  size_t j = 0;
+	  for(size_t i =1; i<a.size();i++){
+		  if(IsGreaterOrEqual(a[i],maxdigit)){
+			  maxdigit = a[i];
+			  j = i;
+		  }
+	  }
+	  b.push_back(maxdigit);
+	  a.erase(a.begin()+j);
+  }
+
+
+
+
   std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
+  for (size_t i = 0; i < b.size(); i++) {
+    ret << b[i];
   }
   string result;
   ret >> result;
